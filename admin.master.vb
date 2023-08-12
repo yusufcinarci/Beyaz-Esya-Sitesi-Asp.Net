775f6d8e9a2d4e124afe854763ef8daba5ea113b
Yusuf çınarcı <yusufcinarci1@gmail.com>
2023-08-12 14:22:40 +0300

    -

--- admin.master.vb
+++
@@ -1,9 +0,0 @@
-﻿
-Partial Class admin
-    Inherits System.Web.UI.MasterPage
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
-    End Sub
-End Class
-
