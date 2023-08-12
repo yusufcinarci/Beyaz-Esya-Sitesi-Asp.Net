620544db7fd0feb42e51f1c4389d71640468f5aa
Yusuf çınarcı <yusufcinarci1@gmail.com>
2023-08-12 14:24:55 +0300

    Sepet

---
+++ Kodlar/Sepet/sepet.aspx.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Partial Class sepet
+    Inherits System.Web.UI.Page
+    Dim baglan As New OleDbConnection("Provider=Microsoft.ACE.OleDB.12.0;Data Source=" & Server.MapPath("App_Data\ders.accdb"))
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        baglan.Open()
+        Dim cmnd As New OleDbCommand
+        cmnd.Connection = baglan
+        cmnd.CommandText = ("delete * from sepet")
+        cmnd.ExecuteNonQuery()
+        baglan.Close()
+
+    End Sub
+End Class
