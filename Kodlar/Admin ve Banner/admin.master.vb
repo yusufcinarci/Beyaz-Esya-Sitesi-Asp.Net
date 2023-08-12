e3ec883d920048e32d967922e2554f163b8f9598
Yusuf çınarcı <yusufcinarci1@gmail.com>
2023-08-12 14:21:09 +0300

    Admin ve Banner

---
+++ Kodlar/Admin ve Banner/admin.master.vb
@@ -0,0 +1,9 @@
+﻿
+Partial Class admin
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+End Class
+
